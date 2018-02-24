9dd4e14bc8ccb5b737f1cde1b3dbbaffb2417ee2
Murphy <lucas.kechouri@epitech.eu>
2018-02-24 23:36:26 +0100

    Auto stash before merge of "master" and "origin/master"
    hand ?

---
+++ cardGame/cardGame/Hand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardGame
+{
+    class Hand
+    {
+        List<Card> cards;
+        Hand(List<Card> cards)
+        {
+            this.cards = cards;
+        }
+
+        public List<Card> getCards()
+        {
+            return cards;
+        }
+
+        public Card Play(int index)
+        {
+            Card tmp = cards[index];
+            cards.Remove(tmp);
+            return (tmp);
+        }
+
+        public void Show()
+        {
+            Card card;
+            int i = 0;
+
+            foreach (var it in cards)
+            {
+                card = it;
+                i += 1;
+                Console.WriteLine("[" + i + "] : " + card.value.ToString() + card.color.ToString());
+            }
+        }
+    }
+}
